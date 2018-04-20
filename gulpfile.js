d225706d18b8c7450a7798b2f6e2f52a6c2b4ce0
Justin Calleja <calleja.justin@gmail.com>
2018-04-20 08:54:43 +0200

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const atImport = require('postcss-import');
+const autoprefixer = require('autoprefixer');
+const simpleVars = require('postcss-simple-vars');
+const del = require('del');
+
+gulp.task('css', function() {
+	const postcssPlugins = [atImport, autoprefixer({ browsers: ['> 1%'], cascade: false }), simpleVars];
+
+	return gulp
+		.src('src/css/index.css')
+		.pipe(postcss(postcssPlugins))
+		.pipe(gulp.dest('build/css'));
+});
+
+gulp.task('watch:css', () => {
+	return gulp.watch('./src/css/**/*.css', ['css']);
+});
+
+gulp.task('clean', function() {
+	return del(['build/**/*']);
+});
+
+gulp.task('default', ['clean', 'css']);
